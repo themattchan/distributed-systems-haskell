56b0e0acc683114c058eab03fba21ad833c100ab
Matthew Chan <matt@themattchan.com>
2016-05-27 20:58:04 -0700

    library imports

--- examples/PingPong1.hs
+++ examples/PingPong1.hs
@@ -1,3 +1,61 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module PingPong1 where
-import qualified Control.Distributed.Process as CH
-import qualified Control.Distributed.Process.Node as CHN
+import Control.Monad ( forever )
+import Control.Concurrent ( threadDelay )
+import Data.Binary
+import Data.Typeable
+import Control.Distributed.Process
+import Control.Distributed.Process.Node
+import Network.Transport.TCP
+-- Serializable (= Binary + Typeable)
+
+-- This file stolen from
+-- https://github.com/pankajmore/cloud-haskell-experiments/blob/master/pingpong.hs
+
+data Ping = Ping (SendPort Pong) deriving (Typeable)
+
+data Pong = Pong deriving (Typeable)
+
+instance Binary Ping where
+    put (Ping sPong) = do put (0 :: Word8)
+                          put sPong
+    get      = do t <- get :: Get Word8
+                  case t of
+                       0 -> do sPong <- get
+                               return (Ping sPong)
+
+instance Binary Pong where
+    put Pong = putWord8 1
+    get      = do { getWord8; return Pong }
+
+server :: ReceivePort Ping -> Process ()
+server rPing = do
+    Ping sPong <- receiveChan rPing
+    liftIO $ putStrLn "Got a ping!"
+    sendChan sPong Pong
+    liftIO $ putStrLn "Sent a pong!"
+    server rPing
+
+client :: SendPort Ping -> Process ()
+client sPing = do
+    (sPong, rPong) <- newChan
+    sendChan sPing (Ping sPong)
+    liftIO $ putStrLn "Sent a ping!"
+    Pong <- receiveChan rPong
+    liftIO $ putStrLn "Got a pong!"
+    client sPing
+
+ignition :: Process ()
+ignition = do
+    -- start the server
+    sPing <- spawnChannelLocal server
+    -- start the client
+    spawnLocal $ client sPing
+    liftIO $ threadDelay 100000 -- wait a while
+
+main :: IO ()
+main = do
+    Right transport <- createTransport "127.0.0.1" "8080"
+          defaultTCPParameters
+    node <- newLocalNode transport initRemoteTable
+    runProcess node ignition
--- src/Cloud.hs
+++ src/Cloud.hs
@@ -1,6 +1,13 @@
 module Cloud
     (
+      -- * Modules
+      module Cloud.Native
+    , module Cloud.Check
     ) where
 
--- import Control.Distributed.Process hiding (Message)
--- import Control.Distributed.Process.Closure
+import Cloud.Embed
+import Cloud.Native
+import Cloud.Check
+
+import Control.Distributed.Process hiding (Message)
+import Control.Distributed.Process.Closure
